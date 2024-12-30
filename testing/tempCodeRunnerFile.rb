39db759368ad9fb0a0398844bb50f1ebc4bcf71b
Ayush-77-77 <ayush95758@gmail.com>
2024-12-30 18:29:13 +0530

    perform unit testing on login authentciation

---
+++ testing/tempCodeRunnerFile.rb
@@ -0,0 +1,24 @@
+
+  # def test_successful_authentication
+  #   email = "test@gmail.com"
+  #   password = "test"
+  #   assert_equal true, @login.authentication?(email, password)
+  # end
+
+  # def test_failed_authentication_due_to_wrong_email
+  #   email = "wrong@gmail.com"
+  #   password = "test"
+  #   assert_equal false, @login.authentication?(email, password)
+  # end
+
+  # def test_failed_authentication_due_to_wrong_password
+  #   email = "test@gmail.com"
+  #   password = "wrong"
+  #   assert_equal false, @login.authentication?(email, password)
+  # end
+
+  # def test_failed_authentication_due_to_both_wrong
+  #   email = "wrong@gmail.com"
+  #   password = "wrong"
+  #   assert_equal false, @login.authentication?(email, password)
+  # end
